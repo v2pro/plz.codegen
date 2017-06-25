95e60f07e5d06c5e236d6f6fbaefce341dca7348
Tao Wen <taowen@gmail.com>
2017-06-25 22:43:30 +0800

    first validation passed

---
+++ result_collector.go
@@ -0,0 +1,79 @@
+package wombat
+
+import (
+	"bytes"
+	"unsafe"
+	"fmt"
+)
+
+type ResultCollector interface {
+	Enter(pathElement interface{}, ptr unsafe.Pointer)
+	Leave()
+	IsVisited(ptr unsafe.Pointer) bool
+	CollectError(err error)
+}
+
+type ValidationError interface {
+	error
+}
+
+type resultCollector struct {
+	path          []interface{}
+	trails        map[unsafe.Pointer]struct{}
+	errors        map[unsafe.Pointer][]error
+	currentErrors []error
+	currentPtr    unsafe.Pointer
+}
+
+func newCollector() *resultCollector {
+	return &resultCollector{
+		path:          []interface{}{},
+		trails:        map[unsafe.Pointer]struct{}{},
+		errors:        map[unsafe.Pointer][]error{},
+		currentErrors: []error{},
+		currentPtr:    unsafe.Pointer(nil),
+	}
+}
+
+func (collector *resultCollector) Enter(pathElement interface{}, ptr unsafe.Pointer) {
+	collector.path = append(collector.path, pathElement)
+	collector.trails[ptr] = struct{}{}
+}
+func (collector *resultCollector) Leave() {
+	collector.path = collector.path[:len(collector.path)-1]
+	if len(collector.currentErrors) > 0 {
+		collector.errors[collector.currentPtr] = collector.currentErrors
+		collector.currentErrors = []error{}
+	}
+	collector.currentPtr = unsafe.Pointer(nil)
+}
+func (collector *resultCollector) IsVisited(ptr unsafe.Pointer) bool {
+	_, visited := collector.trails[ptr]
+	return visited
+}
+func (collector *resultCollector) CollectError(err error) {
+	collector.currentErrors = append(collector.currentErrors, fmt.Errorf("%v: %v", collector.path, err))
+}
+func (collector *resultCollector) result() ValidationError {
+	if len(collector.errors) == 0 {
+		return nil
+	}
+	buf := bytes.Buffer{}
+	for _, subErrors := range collector.errors {
+		for _, err := range subErrors {
+			buf.WriteString(err.Error())
+			buf.WriteByte('\n')
+		}
+	}
+	return &validationError{
+		errmsg: buf.String(),
+	}
+}
+
+type validationError struct {
+	errmsg string
+}
+
+func (err *validationError) Error() string {
+	return err.errmsg
+}
