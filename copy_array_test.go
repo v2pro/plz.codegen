910738ff25ea7bdeb9f615cb4167b2d1cc983bc8
Tao Wen <taowen@gmail.com>
2017-06-30 22:27:59 +0800

    copy json to slice

--- copy_array_test.go
+++ copy_array_test.go
@@ -3,11 +3,20 @@ package wombat
 import (
 	"testing"
 	"github.com/stretchr/testify/require"
+	"github.com/json-iterator/go"
 )
 
 func Test_copy_slice_to_slice(t *testing.T) {
 	should := require.New(t)
 	a := []int{}
 	should.Nil(Copy(&a, []int{1, 2, 3}))
+	should.Equal([]int{1, 2, 3}, a)
+}
+
+func Test_copy_json_to_slice(t *testing.T) {
+	should := require.New(t)
+	b := jsoniter.ParseString(jsoniter.ConfigDefault, `[1,2,3]`)
+	a := []int{}
+	should.Nil(Copy(&a, b))
 	should.Equal([]int{1, 2, 3}, a)
 }
