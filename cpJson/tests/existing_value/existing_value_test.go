569f86da79f5084ab2de90fdc590b5f0113cf83a
Tao Wen <taowen@gmail.com>
2017-07-15 13:53:51 +0800

    cp into existing struct

--- cpJson/tests/existing_value/existing_value_test.go
+++ cpJson/tests/existing_value/existing_value_test.go
@@ -35,6 +35,20 @@ func Test_map_string_ptr_int(t *testing.T) {
 	should.Equal(1, existing)
 }
 
+func Test_struct(t *testing.T) {
+	should := require.New(t)
+
+	type TestObject struct {
+		Field *int
+	}
+
+	existing := int(0)
+	dst := TestObject{Field:&existing}
+	src := `{"Field":1}`
+	should.Nil(jsonCopy(&dst, src))
+	should.Equal(1, existing)
+}
+
 func jsonCopy(dst interface{}, srcJson string) error {
 	iter := jsoniter.ParseString(jsoniter.ConfigDefault, srcJson)
 	return plz.Copy(dst, iter)
