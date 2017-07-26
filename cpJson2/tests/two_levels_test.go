0d9bb105f7d590e793ca585807e612d83ae6de85
Tao Wen <taowen@gmail.com>
2017-07-27 00:31:06 +0800

    cpJson2 support slice+other

--- cpJson2/tests/two_levels_test.go
+++ cpJson2/tests/two_levels_test.go
@@ -21,3 +21,19 @@ func Test_array_slice_int(t *testing.T) {
 func Test_array_struct(t *testing.T) {
 	runFuzzTest(t, reflect.TypeOf([3]model.TypeA{}))
 }
+
+func Test_slice_array_int(t *testing.T) {
+	runFuzzTest(t, reflect.TypeOf([][3]int{}))
+}
+
+func Test_slice_map_string_int(t *testing.T) {
+	runFuzzTest(t, reflect.TypeOf([]map[string]int{}))
+}
+
+func Test_slice_slice_int(t *testing.T) {
+	runFuzzTest(t, reflect.TypeOf([][]int{}))
+}
+
+func Test_slice_struct(t *testing.T) {
+	runFuzzTest(t, reflect.TypeOf([]model.TypeA{}))
+}
